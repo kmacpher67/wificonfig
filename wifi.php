1e5e1ce2ea42b3de488acb493b405c2540f307f9
kmacpher67 <kmacpher67@gmail.com>
2016-02-04 05:11:05 +0000

    fixed up wifi.php sed cut parse into select

--- wifi.php
+++ wifi.php
@@ -2,8 +2,17 @@
     session_start();
     //...your code
     $ssid=$_SESSION['ssid'];
-    $newssid=exec("iwlist wlan0 scanning | awk '/ESSID:/ { print $1;}' | cut -d ':' -f 2-");
-    echo $newssid
+    $newssid = shell_exec("iwlist wlan0 scanning | awk '/ESSID:/ { print $1;}' | cut -d ':' -f 2- | sed -e 's/^\"//'  -e 's/\"$//'");
+    $newssid2 = shell_exec("iwlist wlan0 scanning | awk '/ESSID:/ { print $1;}'");
+    // echo shell_exec("iwlist wlan0 scanning | awk '/ESSID:/ { print $1;}' | cut -d ':' -f 2-");
+    //echo "<hr>";
+    //echo "<!-- " + $newssid + " --!>";
+
+
     //... $_SESSION['ssid']=$ssid;
     ?>
-
+<BR>
+ 	<?php
+	foreach(preg_split("/((\r?\n)|(\r\n?))/", $newssid) as $line){ ?>
+           <option value="<?php echo $line; ?>"><?php echo $line; ?></option> 
+	<?php } ?>
