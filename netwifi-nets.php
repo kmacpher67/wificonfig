e47afb4837f2d51a479b8930d4754eb5d40d9464
kmacpher67 <kmacpher67@gmail.com>
2016-02-13 14:23:50 -0500

    networks on interface

---
+++ netwifi-nets.php
@@ -0,0 +1,22 @@
+<?php
+// get networks on $interface 
+
+require_once 'Net/Wifi.php';
+$wifi = new Net_Wifi();
+
+if (is_null($interface)|| $interface==""){
+      $interface="wlan0";
+      }
+
+$networks = $wifi->scan($interface);
+
+if (count($networks) == 0) {
+
+    echo '<div class="ERROR"> No wireless networks available.</div>' . "\r\n";
+
+    exit();
+
+}
+
+?>
+
