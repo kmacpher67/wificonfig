e33580da0839ef518e552af6272cad0f06f837a8
kmacpher67 <kmacpher67@gmail.com>
2016-02-13 13:51:17 -0500

    lookup wlan0 interface

---
+++ netwifi-interfaces.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Net/Wifi.php';
+$wifi = new Net_Wifi();
+//get all wireless interfaces
+
+$interfaces = $wifi->getSupportedInterfaces();
+
+if (count($interfaces) == 0) {
+
+    echo '<div class="Error"> No wireless interfaces found! </div>' . "\r\n";
+
+    exit();
+
+}
+$wlanvalid=false; 
+foreach ($interfaces as $interface1) {
+
+   if(strcmp($interface1,"wlan0")==0){ 
+     echo '<!---  Wireless interface: ' . $interface1 . "  FOUND! --!>";
+     $wlanvalid=true;
+     break;
+   }
+
+}
+
+
+?>
+
