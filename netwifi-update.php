5a2b16dc7870988ea20aa737707e04b2bb0d0e9e
kmacpher67 <kmacpher67@gmail.com>
2016-02-17 08:34:45 -0500

    wifi network deatils

--- netwifi-update.php
+++ netwifi-update.php
@@ -1,11 +1,23 @@
 <?php
-
 // update processing form for restart refresh wifi adapter. 
 
+require_once 'Net/Wifi.php';
+$wifi = new Net_Wifi();
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+if (is_null($interface)|| $interface==""){
+      $interface="wlan0";
+      }
+
+$networks = $wifi->scan($interface);
 
 if( $_POST["ssid"] || $_POST["keyboard"] ) {
       echo "<!-- " . $_POST["ssid"]  .  $_POST["keyboard"] . " ---!>"; 
-
+       
+      
       }
 
 include 'config.php';
