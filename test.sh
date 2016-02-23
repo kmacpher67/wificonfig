f055a8b34a0cc44e5f9cceef7686393175b501d4
kmacpher67 <kmacpher67@gmail.com>
2016-02-23 15:53:00 -0500

    edit test.sh changed status to add sudo

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ sudo ls -ltr *.php
 
 sudo wpa_cli -iwlan0 status
 
-sudo ifdown wlan0
 sudo wpa_cli -iwlan0 select_network 0
 sudo wpa_cli -iwlan0 select_network 0
 sudo wpa_cli -iwlan0 reassociate
-sudo wpa_cli -iwlan0 save_config
 sudo wpa_cli -iwlan0 status 
-sleep 1
+
+sudo ifdown wlan0
+sleep 3
 sudo ifup wlan0
 sudo wpa_cli -iwlan0 list_networks
